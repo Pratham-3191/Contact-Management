bd70e62078b31ccb77f26bdc5af0447fe6711f1b
Pratham Chaudhari <prathamchaudhari124@gmail.com>
2026-01-03 01:46:52 +0530

    typo fix

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const contactRoutes = require("./routes/contactRoutes");
+const contactRoutes = require("./routes/ContactRoutes");
 
 const app = express();
 
